eedcc50c73d7e47b99ef31d4832e5e0760505c27
Deuterrr <bagaspri4111@gmail.com>
2026-04-24 15:09:08 +0700

    refactor(service-core): add fee type on payment method domain
    - add fee type such as flat, percentage, and mixed
    - change from uppercase string to lower case so both system and i/o result can easily be compared

--- service-core/internal/modules/payment/domain/payment_method.go
+++ service-core/internal/modules/payment/domain/payment_method.go
@@ -7,19 +7,29 @@ import (
 )
 
 type PaymentMethodType string
+type PaymentFeeType string
 
 const (
-	TypeBankTransfer PaymentMethodType = "BANK_TRANSFER"
-	TypeEWallet      PaymentMethodType = "EWALLET"
-	TypeQRCode       PaymentMethodType = "QR_CODE"
+	TypeBankTransfer PaymentMethodType = "bank_transfer"
+	TypeEWallet      PaymentMethodType = "ewallet"
+	TypeQRCode       PaymentMethodType = "qr_code"
+
+	FeeTypeFlat       PaymentFeeType = "flat"
+	FeeTypePercentage PaymentFeeType = "percentage"
+	FeeTypeMixed      PaymentFeeType = "mixed"
 )
 
 type PaymentMethod struct {
-	ID          uuid.UUID
+	ID uuid.UUID
+
 	Name        string
-	Type        string
+	Type        PaymentMethodType
 	IsActive    bool
 	Description string
+
+	FeeType       PaymentFeeType
+	FeeFixed      int64
+	FeePercentage float64
 
 	CreatedAt time.Time
 	UpdatedAt *time.Time
