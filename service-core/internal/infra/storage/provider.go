add41058cf9d49b5a2784f61bd06c12a3f300212
Deuterrr <bagaspri4111@gmail.com>
2026-05-09 17:25:07 +0700

    refactor: divide multi contracts on provider (#117)
    - now Provider has mupltiple contract for different tasks:
    >>  - ObjectResponse for global response of object (file)
    >>  - ObjectStorage as global method interface for requesting object from storage
    >>  - URLResolver as global method interface for validating url
    >>  - BucketManager as global method for checking whether the bucket exist or not, (migration purpose)

--- service-core/internal/infra/storage/provider.go
+++ service-core/internal/infra/storage/provider.go
@@ -4,15 +4,35 @@ import (
 	"io"
 )
 
-type Provider interface {
-	Upload(input UploadInput) (Object, error)
-	Delete(key string) error
-	Exists(key string) (bool, error)
-}
-
 type UploadInput struct {
 	Key           string
 	File          io.Reader
 	ContentType   string
 	ContentLength int64
+}
+
+type ObjectResponse struct {
+	Key         string
+	ContentType string
+}
+
+type ObjectStorage interface {
+	Upload(input UploadInput) (*ObjectResponse, error)
+	Delete(key string) error
+	Exists(key string) (bool, error)
+}
+
+type URLResolver interface {
+	PublicURL(key string) string
+	SignedURL(key string) (string, error)
+}
+
+type BucketManager interface {
+	EnsureBucket() error
+}
+
+type Provider interface {
+	ObjectStorage
+	URLResolver
+	BucketManager
 }
