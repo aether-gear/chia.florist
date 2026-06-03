ff6a73333a5fed710b6447f02ce4317856dbda91
Deuterrr <bagaspri4111@gmail.com>
2026-06-03 09:15:09 +0700

    feat: add transaction contract (#102)
    Add both contract and implementation of it

---
+++ service-core/internal/infra/db/transactor.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+
+	"service-core/internal/shared/transaction"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresTransactor struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresTransactor(db *pgxpool.Pool) transaction.Transactor {
+	return &PostgresTransactor{
+		db: db,
+	}
+}
+
+func (t *PostgresTransactor) WithinTransaction(
+	ctx context.Context,
+	fn func(transaction.Executor) error,
+) error {
+	tx, err := t.db.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	if err := fn(tx); err != nil {
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
