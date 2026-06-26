6432634602aaa96b54b8c35ae073fe58c615c462
Deuterrr <bagaspri4111@gmail.com>
2026-06-26 22:09:30 +0700

    feat: add customer id to authentication

--- service-core/internal/modules/authentication/domain/claim.go
+++ service-core/internal/modules/authentication/domain/claim.go
@@ -24,9 +24,10 @@ const (
 // Instead, it should be translated into an AuthContext after the token
 // has been successfully validated.
 type TokenClaims struct {
-	UserID    uuid.UUID
-	SessionID uuid.UUID
-	StaffID   *uuid.UUID
+	UserID     uuid.UUID
+	SessionID  uuid.UUID
+	StaffID    *uuid.UUID
+	CustomerID *uuid.UUID
 
 	Type TokenType
 
