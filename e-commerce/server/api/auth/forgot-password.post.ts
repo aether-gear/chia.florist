03edaf75446f64d6c1f9335c86f3ed8ca5d58471
Rayhan Shidqi <riutotakato06@gmail.com>
2026-07-09 02:15:02 +0700

    feat: implement forgot and reset password #250
    also add new password and verify otp

---
+++ e-commerce/server/api/auth/forgot-password.post.ts
@@ -0,0 +1,21 @@
+import { readBody } from 'h3'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig()
+  const backendUrl = `${config.public.serviceCoreApiUrl}/auth/forgot-password`
+  const body = await readBody(event)
+
+  try {
+    const response = await $fetch.raw(backendUrl, {
+      method: 'POST',
+      body,
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    return response._data
+  } catch (err: any) {
+    event.node.res.statusCode = err.status || 500
+    return err.data || { message: err.message }
+  }
+})
